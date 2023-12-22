b487faf0d21e293c98d6753d4a05d0cfee927807
Danny <dannyhdyt14@gmail.com>
2023-12-22 17:58:25 +0700

    edit dependency pom.xml dan edit java

--- src/main/java/automation/switchframe/AutomationTest.java
+++ src/main/java/automation/switchframe/AutomationTest.java
@@ -4,24 +4,24 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.time.Duration;
 
 
-public class switchFrame {
+public class AutomationTest {
     WebDriver driver;
 
     @Test
     public void yopmailIFrame() {
+
         //open browser
-        WebDriverManager.firefoxdriver();
-        driver = new FirefoxDriver();
+        WebDriverManager.chromedriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://yopmail.com");
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         //Enter inbox
@@ -31,8 +31,18 @@ public class switchFrame {
 
         //extract text
         driver.switchTo().frame("ifmail");
-        String actualEmail = driver.findElement(By.id("mail")).getText();
-        System.out.println("----this is the first email text content--- \n" + actualEmail);
+
+        // show body email content
+        String emailContentText = driver.findElement(By.tagName("body")).getText();
+        System.out.println(emailContentText);
+
+        // Assert one of the email content text
+        String txtActualContent = driver.findElement(By.tagName("strong")).getText();
+        String txtExpectedContent = "Thanks for connecting!";
+        System.out.println("----------------------");
+        System.out.println(txtExpectedContent);
+        System.out.println("----------------------");
+        Assert.assertEquals(txtActualContent, txtExpectedContent);
         driver.switchTo().parentFrame();
     }
 }
